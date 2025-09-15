352886f7484ea56453ffed70c076a845cba7ed3f
Gaissuhfi <bobo218079@gmail.com>
2025-09-15 20:56:52 +0800

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -10,7 +10,7 @@ const navItems = [
   { name: "About", href: "#about" },
   { name: "Skills", href: "#skills" },
   { name: "Projects", href: "#projects" },
-  { name: "Blog", href: "#blog" },
+  { name: "Blog", href: "#blogs" },
   { name: "Experience", href: "#experience" },
   { name: "Contact", href: "#contact" },
 ]
